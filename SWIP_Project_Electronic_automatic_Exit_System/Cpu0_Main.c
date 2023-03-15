1d0984116d941e1b711d1da6d833b935043f38b9
HyunSwim <coldsz@naver.com>
2023-03-15 14:50:15 +0900

    complete to make LED_logic

--- SWIP_Project_Electronic_automatic_Exit_System/Cpu0_Main.c
+++ SWIP_Project_Electronic_automatic_Exit_System/Cpu0_Main.c
@@ -125,6 +125,50 @@ void initGTM(void);
 //    P10_OUT.U ^= 0x1 << P2_BIT_LSB_IDX;
 //}
 
+__interrupt(0x0A) __vector_table(0)
+void ERU0_ISR(void)
+{
+    if( (P00_IN.U & (0x1 << P4_BIT_LSB_IDX)) != 0 )     // rising edge of echo
+    {
+        //                    _________
+        //        echo ______|
+        //                   ^
+        CCU61_TCTR4.U = 0x1 << T12RS_BIT_LSB_IDX;       // start CCU61 T12 counter
+    }
+    else                                            // falling edge of echo
+    {
+        //             ______
+        //        echo       |______
+        //                   ^
+
+        CCU61_TCTR4.B.T12RR = 0x1;       // stop CCU61 T12 counter
+
+        // (1 / t_freq) * counter * 1000000 / 58  =  centimeter
+        range = ((CCU61_T12.B.T12CV * 1000000) / 48828) / 58;
+        range_valid_flag = 1;
+
+        CCU61_TCTR4.B.T12RES = 0x1;       // reset CCU61 T12 counter
+    }
+}
+
+
+__interrupt(0x0B) __vector_table(0)
+void CCU60_T12_ISR(void)
+{
+    // end of 10us Trig
+    // GPIO P02.6 --> LOW
+    P02_OUT.U &= ~(0x1 << P6_BIT_LSB_IDX);
+}
+
+__interrupt(0x0C) __vector_table(0)
+void CCU60_T13_ISR(void)
+{
+    P10_OUT.U ^= 0x1 << P2_BIT_LSB_IDX;
+    P02_IOCR0.B.PC3 ^= 0x11;
+}
+
+
+
 int core0_main(void)
 {
     IfxCpu_enableInterrupts();
@@ -139,7 +183,12 @@ int core0_main(void)
     IfxCpu_emitEvent(&g_cpuSyncEvent);
     IfxCpu_waitEvent(&g_cpuSyncEvent, 1);
 
-    //initERU();
+    initERU();
+    initCCU60();
+    initCCU61();
+    initLED();
+    //initRGBLED();
+    //initVADC();
     initMotor(); ///////added
     initGTM();
     initVADC();
@@ -179,8 +228,154 @@ int core0_main(void)
 
 void initMotor(void)
 {
-    P10_IOCR0.U &= ~(0x1F << PC1_BIT_LSB_IDX);
-    P02_IOCR0.U &= ~(0x1F << PC1_BIT_LSB_IDX);
+    P10_IOCR0.U &= ~(0x1F << PC2_BIT_LSB_IDX);     // reset P10_IOCR0 PC2
+
+    P10_IOCR0.U |= 0x10 << PC2_BIT_LSB_IDX;        // set P10.2 push-pull general output
+}
+
+void initButton(void)
+{
+    P02_IOCR0.U &= ~(0x1F << PC1_BIT_LSB_IDX);       // reset P02_IOCR0 PC1
+
+    P02_IOCR0.U |= 0x02 << PC1_BIT_LSB_IDX;          // set P02.1 general input (pull-up connected)
+}
+
+void initERU(void)
+{
+    // ERU setting
+    SCU_EICR1.U &= ~(0x7 << EXIS0_BIT_LSB_IDX);
+
+    SCU_EICR1.U |= (0x2  << EXIS0_BIT_LSB_IDX);     // ERS2 - Input 2
+
+
+    SCU_EICR1.U |= 0x1   << FEN0_BIT_LSB_IDX;       // falling edge
+
+    SCU_EICR1.U |= 0x1   << REN0_BIT_LSB_IDX;       // rising edge
+
+
+    SCU_EICR1.U |= 0x1   << EIEN0_BIT_LSB_IDX;
+
+    SCU_EICR1.U &= ~(0x7 << INP0_BIT_LSB_IDX);
+
+    SCU_IGCR0.U &= ~(0x3 << IGP0_BIT_LSB_IDX);
+    SCU_IGCR0.U |= 0x1   << IGP0_BIT_LSB_IDX;
+
+    // SRC Interrupt setting
+    SRC_SCU_SCU_ERU0.U &= ~(0xFF << SRPN_BIT_LSB_IDX);
+    SRC_SCU_SCU_ERU0.U |= 0x0A   << SRPN_BIT_LSB_IDX;
+
+    SRC_SCU_SCU_ERU0.U &= ~(0x3  << TOS_BIT_LSB_IDX);
+
+    SRC_SCU_SCU_ERU0.U |= 1      << SRE_BIT_LSB_IDX;
+}
+
+void initCCU60(void)
+{
+    // Password Access to unlock SCU_WDTSCON0
+    SCU_WDTCPU0_CON0.U = ((SCU_WDTCPU0_CON0.U ^ 0xFC) & ~(1 << LCK_BIT_LSB_IDX)) | (1 << ENDINIT_BIT_LSB_IDX);
+    while((SCU_WDTCPU0_CON0.U & (1 << LCK_BIT_LSB_IDX)) != 0);    // wait until unlocked
+
+    // Modify Access to clear ENDINIT
+    SCU_WDTCPU0_CON0.U = ((SCU_WDTCPU0_CON0.U ^ 0xFC) | (1 << LCK_BIT_LSB_IDX)) & ~(1 << ENDINIT_BIT_LSB_IDX);
+    while((SCU_WDTCPU0_CON0.U & (1 << LCK_BIT_LSB_IDX)) == 0);    // wait until locked
+
+    CCU60_CLC.U &= ~(1 << DISR_BIT_LSB_IDX);    // enable CCU
+
+    // Password Access to unlock SCU_WDTSCON0
+    SCU_WDTCPU0_CON0.U = ((SCU_WDTCPU0_CON0.U ^ 0xFC) & ~(1 << LCK_BIT_LSB_IDX)) | (1 << ENDINIT_BIT_LSB_IDX);
+    while((SCU_WDTCPU0_CON0.U & (1 << LCK_BIT_LSB_IDX)) != 0);    // wait until unlocked
+
+    // Modify Access to set ENDINIT
+    SCU_WDTCPU0_CON0.U = ((SCU_WDTCPU0_CON0.U ^ 0xFC) | (1 << LCK_BIT_LSB_IDX)) | (1 << ENDINIT_BIT_LSB_IDX);
+    while((SCU_WDTCPU0_CON0.U & (1 << LCK_BIT_LSB_IDX)) == 0);    // wait until locked
+
+    // CCU60 T12 configurations
+    while((CCU60_CLC.U & (1 << DISS_BIT_LSB_IDX)) != 0);// wait until CCU60 module enabled
+
+
+    CCU60_TCTR0.U &= ~(0x7 << T12CLK_BIT_LSB_IDX);      // f_T12 = f_CCU6 / prescaler
+    CCU60_TCTR0.U |= 0x2 << T12CLK_BIT_LSB_IDX;         // f_CCU6 = 50 MHz, prescaler = 4
+    //CCU60_TCTR0.U |= 0x1 << T12PRE_BIT_LSB_IDX;         // f_T12 --> 12.5 MHz
+
+
+    CCU60_TCTR0.U &= ~(0x1 << CTM_BIT_LSB_IDX);         // T12 auto reset when period match (PM) occur
+
+
+    CCU60_T12PR.U = 125 -1;                       // PM interrupt freq. = f_T12 / (T12PR + 1)
+    CCU60_TCTR4.U |= 0x1 << T12STR_BIT_LSB_IDX;         // load T12PR from shadow register
+
+
+    CCU60_TCTR2.B.T12SSC = 0x1;                     // Single Shot Control
+
+
+    CCU60_T12.U = 0;                                // clear T12 counter register
+
+
+    // CCU60 T12 PM interrupt setting
+    CCU60_INP.U &= ~(0x3 << INPT12_BIT_LSB_IDX);        // service request output SR0 selected
+    CCU60_IEN.U |= 0x1 << ENT12PM_BIT_LSB_IDX;          // enable T12 PM interrupt
+
+
+    // SRC setting for CCU60
+    SRC_CCU6_CCU60_SR0.U &= ~(0xFF << SRPN_BIT_LSB_IDX);
+    SRC_CCU6_CCU60_SR0.U |= 0x0B << SRPN_BIT_LSB_IDX;   // set priority 0x0B
+
+    SRC_CCU6_CCU60_SR0.U &= ~(0x3 << TOS_BIT_LSB_IDX);  // CPU0 service T12 PM interrupt
+
+    SRC_CCU6_CCU60_SR0.U |= 0x1 << SRE_BIT_LSB_IDX;     // SR0 enabled
+
+    CCU60_TCTR0.B.T13CLK = 0x2;     // f_CCU6 = 50 MHz, prescaler = 1024
+    CCU60_TCTR0.B.T13PRE = 0x1;     // prescaler enable
+    CCU60_T13PR.B.T13PV = 125 - 1;
+    CCU60_TCTR4.B.T13STR = 0x1;
+    CCU60_T13.B.T13CV = 0x0;
+    CCU60_IEN.B.ENT13PM = 0x1;
+    CCU60_INP.B.INPT13 = 0x1;       // SR1
+    SRC_CCU6_CCU60_SR1.B.SRPN = 0x0C;
+    SRC_CCU6_CCU60_SR1.B.TOS = 0x0;
+    SRC_CCU6_CCU60_SR1.B.SRE = 0x1;
+    CCU60_TCTR4.B.T13RS = 0x1;
+}
+
+void initCCU61(void)
+{
+    // Password Access to unlock SCU_WDTSCON0
+    SCU_WDTCPU0_CON0.U = ((SCU_WDTCPU0_CON0.U ^ 0xFC) & ~(1 << LCK_BIT_LSB_IDX)) | (1 << ENDINIT_BIT_LSB_IDX);
+    while((SCU_WDTCPU0_CON0.U & (1 << LCK_BIT_LSB_IDX)) != 0);    // wait until unlocked
+
+    // Modify Access to clear ENDINIT
+    SCU_WDTCPU0_CON0.U = ((SCU_WDTCPU0_CON0.U ^ 0xFC) | (1 << LCK_BIT_LSB_IDX)) & ~(1 << ENDINIT_BIT_LSB_IDX);
+    while((SCU_WDTCPU0_CON0.U & (1 << LCK_BIT_LSB_IDX)) == 0);    // wait until locked
+
+    CCU61_CLC.U &= ~(1 << DISR_BIT_LSB_IDX);    // enable CCU
+
+    // Password Access to unlock SCU_WDTSCON0
+    SCU_WDTCPU0_CON0.U = ((SCU_WDTCPU0_CON0.U ^ 0xFC) & ~(1 << LCK_BIT_LSB_IDX)) | (1 << ENDINIT_BIT_LSB_IDX);
+    while((SCU_WDTCPU0_CON0.U & (1 << LCK_BIT_LSB_IDX)) != 0);    // wait until unlocked
+
+    // Modify Access to set ENDINIT
+    SCU_WDTCPU0_CON0.U = ((SCU_WDTCPU0_CON0.U ^ 0xFC) | (1 << LCK_BIT_LSB_IDX)) | (1 << ENDINIT_BIT_LSB_IDX);
+    while((SCU_WDTCPU0_CON0.U & (1 << LCK_BIT_LSB_IDX)) == 0);    // wait until locked
+
+    // CCU60 T12 configurations
+    while((CCU61_CLC.U & (1 << DISS_BIT_LSB_IDX)) != 0);// wait until CCU60 module enabled
+
+
+    CCU61_TCTR0.U &= ~(0x7 << T12CLK_BIT_LSB_IDX);      // f_T12 = f_CCU6 / prescaler = 12.5 MHz
+    CCU61_TCTR0.U |= 0x2 << T12CLK_BIT_LSB_IDX;         // f_CCU6 = 50 MHz, prescaler = 4
+
+    CCU61_TCTR0.U |= 0x1 << T12PRE_BIT_LSB_IDX;         // f_T12 = f_CCU6 / 256 = 48,828 Hz
+
+
+    CCU61_T12PR.U = 100000 -1;                       // PM interrupt freq. = f_T12 / (T12PR + 1)
+    CCU61_TCTR4.U |= 0x1 << T12STR_BIT_LSB_IDX;         // load T12PR from shadow register
+
+    CCU61_T12.U = 0;                                // clear T12 counter register
+}
+
+void initRGBLED(void)
+{
+    // reset Port IOCR register
     P02_IOCR4.U &= ~(0x1F << PC7_BIT_LSB_IDX);
 
     P10_IOCR0.U |= 0x10 << PC1_BIT_LSB_IDX;
